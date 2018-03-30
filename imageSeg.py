d679772a6eed63f9c9ea859c5637c97f60bc0076
jimingh <jimingh@gmail.com>
2018-03-30 09:32:12 +0800

    Add files via upload

--- imageSeg.py
+++ imageSeg.py
@@ -11,7 +11,7 @@ from utils import visualization_utils as vis_util
 if tf.__version__ < '1.4.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
   
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
 def load_image(image):
